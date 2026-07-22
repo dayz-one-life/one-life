b57c2d979853ac0170f7b845f3bdc9bd3da2f900
Steve Manicdote <steve@manicdotes.com>
2026-07-21 17:07:37 -0700

    feat(friends): pair ordering and viewer-relative projection

---
+++ packages/friends/src/index.ts
@@ -0,0 +1,6 @@
+export {
+  orderPair, viewOf, cooldownEnd,
+  FRIEND_REQUEST_COOLDOWN_DAYS, FRIEND_REQUEST_DAILY_LIMIT,
+} from "./pair.js";
+export type { FriendshipRow, FriendStatus, FriendView } from "./pair.js";
+export { FriendError } from "./errors.js";
